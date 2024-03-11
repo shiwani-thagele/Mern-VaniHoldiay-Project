9bb08eb70d618d9ff09d28df27b7ac18ae13127d
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2024-03-11 20:08:50 +0530

    mern stack hotel system header,footer and signin and sign out page design and connect with mongodb.

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+// import { useAppContext } from "../contexts/AppContext";
+// import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+//   const { isLoggedIn } = useAppContext();
+
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">VaniHolidays.com</Link>
+        </span>
+      <span className="flex space-x-2 justify-item-center">
+        <Link
+          to="/sign-in"
+          className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+        >
+          Sign In
+        </Link>
+       
+      </span>
+    </div>
+  </div>
+  );
+};
+
+export default Header;
