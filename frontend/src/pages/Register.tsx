9578040addeb9b240a631bd389ee00c4bb763164
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2024-03-13 02:16:58 +0530

    login form api

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,35 @@
+import {useForm} from "react-hook-form";
+type RegisterFormData ={
+    firstName:string,
+    lastName:string,
+    email:string,
+    password:string,
+    confirmPassword:string,
+
+}
+const Register = () =>{
+
+ const{ register } = useForm<RegisterFormData>();
+
+
+    return(
+        <form className="flex flex-col gap-5">
+            <h2 className="text-3xl font-bold">Create an Account</h2>
+            <div className="flex flex-col md:flex-row gap-5">
+                <label className=" text-grey-700 text-sm font-bold ">
+                    First Name
+                  <input className="border rounded w-full py-1 px-2 font-normal"{...register("firstName",{required:"This filed is required"})}></input>
+                </label>
+
+                <label className=" text-grey-700 text-sm font-bold flex-1">
+                    Last Name
+                  <input className="border rounded w-full py-1 px-2 font-normal"></input>
+                </label>
+
+
+            </div>
+        </form>
+    );
+};
+
+export default Register;
