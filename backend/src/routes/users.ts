25b54b0028de3552f517b3da8147c5113a74b205
shiwani thagele <78925065+shiwani-thagele@users.noreply.github.com>
2024-03-12 22:06:43 +0530

    token authentication

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,16 +1,29 @@
 import express,{Request,Response} from 'express';
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 const router = express.Router();
 
+
 // /api/users/register
-router.post("/register",async (req:Request,res:Response) => {
+router.post("/register",
+[
+   check("firstname","First Name is required").isString(),
+   check("lastname","Last Name is required").isString(),
+   check("email","Email is required").isString(),
+   check("password","Password with 6 or more characters required").isLength({min:6,}),
+
+],
+async (req:Request,res:Response) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()){
+      return res.status(400).json({message:errors.array()});
+   }
        try{
          let user = await User.findOne({
             email:req.body.email,
          })
-
-         if(user){
+       if(user){
             return res.status(400).json({message:"User already exists"})
          }
          user = new User(req.body);
